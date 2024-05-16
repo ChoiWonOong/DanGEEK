c1d6ae8985e77be4778f51e8d1be02788e66c3be
ChoiWonOong <dnd5301@gmail.com>
2024-05-16 14:56:38 +0900

    FIX: Erase Duplicate Websocket Connector

--- src/main/java/DanGEEK/app/config/WebSocketConfig.java
+++ src/main/java/DanGEEK/app/config/WebSocketConfig.java
@@ -7,14 +7,12 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Configuration
-@EnableWebSocket    // 웹소켓 서버 사용
 @EnableWebSocketMessageBroker   // STOMP 사용
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -28,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry
-                .addEndpoint("/ws-stomp")     // 엔드포인트: /ws
+                .addEndpoint("/chat")     // 엔드포인트: /ws
                 .setAllowedOriginPatterns("*");
     }
     @Override
